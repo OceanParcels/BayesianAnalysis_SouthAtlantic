12101bbc66b63eb04501282bad997ed0ce6f5f78
cpierard <c.m.pierard@uu.nl>
2021-03-29 18:33:50 +0200

    Update corrected kernels with kernels stored in local_kernels.py to keep the code short

--- SAG_experiment.py
+++ SAG_experiment.py
@@ -6,6 +6,8 @@ import numpy as np
 import sys
 from parcels import ParcelsRandom
 import math
+from parcels import GeographicPolar, Geographic
+import local_kernels as kernels
 
 resusTime = 69
 shoreTime = 10
@@ -18,7 +20,6 @@ n_days = 1600  # number of days to simulate
 K_bar = 10  # diffusion value
 stored_dt = 24  # hours
 loc = sys.argv[1]
-repeatdt = timedelta(days=10)
 
 # data = '../data/mercatorpsy4v3r1_gl12_mean_20180101_R20180110.nc'
 data = '/data/oceanparcels/input_data/CMEMS/' + \
@@ -50,8 +51,8 @@ fieldset.add_field(Field('borU', data=u_border,
 fieldset.add_field(Field('borV', data=v_border,
                          lon=fieldset.U.grid.lon, lat=fieldset.U.grid.lat,
                          mesh='spherical'))
-# fieldset.borU.units = GeographicPolar()
-# fieldset.borV.units = Geographic()
+fieldset.borU.units = GeographicPolar()
+fieldset.borV.units = Geographic()
 
 ###############################################################################
 # Adding in the  land cell identifiers                                        #
@@ -106,8 +107,6 @@ class SimpleBeachingResuspensionParticle(JITParticle):
     distance = Variable('distance', dtype=np.float32, initial=0)
 
 
-# CHEEEECKKKK!!!!!
-print('Check 2')
 #####################################
 # Opening file with positions and sampling dates.
 river_sources = np.load('river_sources.npy', allow_pickle=True).item()
@@ -140,124 +139,16 @@ pset = ParticleSet.from_list(fieldset=fieldset,
 
 
 ###############################################################################
-# KERNELS
+# And now the overall kernel                                                  #
 ###############################################################################
-def delete_particle(particle, fieldset, time):  # indices=indices):
+def delete_particle(particle, fieldset, time):
     particle.delete()
 
 
-def beach(particle, fieldset, time):
-    if particle.beach == 0:
-        dist = fieldset.distance2shore[time, particle.depth, particle.lat,
-                                       particle.lon]
-        if dist < 10:
-            beach_prob = math.exp(-particle.dt/(particle.coastPar*86400.))
-            if ParcelsRandom.random(0., 1.) > beach_prob:
-                particle.beach = 1
-    # Now the part where we build in the resuspension
-    elif particle.beach == 1:
-        resus_prob = math.exp(-particle.dt/(particle.resus_t*86400.))
-        if ParcelsRandom.random(0., 1.) > resus_prob:
-            particle.beach = 0
-    # Update the age of the particle
-    particle.age += particle.dt
-
-
-def AntiBeachNudging(particle, fieldset, time):
-    """
-    The nudging current is 1 m s^-1, which ought to be sufficient to overpower
-    any coastal current (I hope) and push our particle back out to sea so as to
-    not get stuck
-
-    update 11/03/2020: Following tests and discussions with Cleo, the nudging
-    current will now kick in starting at 500m from the coast, since otherwise
-    the particles tended to get stuck if we used the velocity treshhold.
-    """
-
-    if fieldset.distance2shore[time, particle.depth,
-                               particle.lat, particle.lon] < 0.5:
-        borUab = fieldset.borU[time, particle.depth, particle.lat,
-                               particle.lon]
-        borVab = fieldset.borV[time, particle.depth, particle.lat,
-                               particle.lon]
-        particle.lon -= borUab*particle.dt
-        particle.lat -= borVab*particle.dt
-
-
-def AdvectionRK4_floating(particle, fieldset, time):
-    """Advection of particles using fourth-order Runge-Kutta integration.
-    Function needs to be converted to Kernel object before execution
-
-    A particle only moves if it has not beached (rather obviously)
-    """
-    if particle.beach == 0:
-        particle.distance = fieldset.distance2shore[time, particle.depth,
-                                                    particle.lat, particle.lon]
-
-        # if particle.lon > 180:
-        #     particle.lon -= 360
-        # if particle.lon < -180:
-        #     particle.lon += 360
-        (u1, v1) = fieldset.UV[time, particle.depth, particle.lat,
-                               particle.lon]
-        lon1, lat1 = (particle.lon + u1*.5*particle.dt,
-                      particle.lat + v1*.5*particle.dt)
-        # if lon1 > 180:
-        #     lon1 -= 360
-        # if lon1 < -180:
-        #     lon1 += 360
-        (u2, v2) = fieldset.UV[time + .5 * particle.dt, particle.depth,
-                               lat1, lon1]
-        lon2, lat2 = (particle.lon + u2*.5*particle.dt,
-                      particle.lat + v2*.5*particle.dt)
-        # if lon2 > 180:
-        #     lon2 -= 360
-        # if lon2 < -180:
-        #     lon2 += 360
-        (u3, v3) = fieldset.UV[time + .5 * particle.dt, particle.depth,
-                               lat2, lon2]
-        lon3, lat3 = (particle.lon + u3*particle.dt,
-                      particle.lat + v3*particle.dt)
-
-        # if lon3 > 180:
-        #     lon3 -= 360
-        # if lon3 < -180:
-        #     lon3 += 360
-        (u4, v4) = fieldset.UV[time + particle.dt, particle.depth, lat3, lon3]
-
-        particle.lon += (u1 + 2*u2 + 2*u3 + u4) / 6. * particle.dt
-        # if particle.lon > 180:
-        #     particle.lon -= 360
-        # if particle.lon < -180:
-        #     particle.lon += 360
-        particle.lat += (v1 + 2*v2 + 2*v3 + v4) / 6. * particle.dt
-
-
-def BrownianMotion2D(particle, fieldset, time):
-    """Kernel for simple Brownian particle diffusion in zonal and meridional
-    direction. Assumes that fieldset has fields Kh_zonal and Kh_meridional
-    we don't want particles to jump on land and thereby beach"""
-    if particle.beach == 0:
-        r = 1/3.
-        kh_meridional = fieldset.Kh_meridional[time, particle.depth,
-                                               particle.lat, particle.lon]
-        lat_p = particle.lat + ParcelsRandom.uniform(-1., 1.) * \
-            math.sqrt(2*math.fabs(particle.dt)*kh_meridional/r)
-        kh_zonal = fieldset.Kh_zonal[time, particle.depth,
-                                     particle.lat, particle.lon]
-        lon_p = particle.lon + ParcelsRandom.uniform(-1., 1.) * \
-            math.sqrt(2*math.fabs(particle.dt)*kh_zonal/r)
-        particle.lon = lon_p
-        particle.lat = lat_p
-
-
-###############################################################################
-# And now the overall kernel                                                  #
-###############################################################################
-totalKernel = pset.Kernel(AdvectionRK4_floating) + \
-    pset.Kernel(BrownianMotion2D) + pset.Kernel(AntiBeachNudging) + \
-    pset.Kernel(beach)
-
+totalKernel = pset.Kernel(kernels.AdvectionRK4_floating) + \
+    pset.Kernel(kernels.BrownianMotion2D) + \
+    pset.Kernel(kernels.AntiBeachNudging) + \
+    pset.Kernel(kernels.beach)
 
 # Output file
 output_file = pset.ParticleFile(
---
+++ local_kernels.py
@@ -0,0 +1,82 @@
+from parcels import ParcelsRandom
+import math
+
+
+def beach(particle, fieldset, time):
+
+    if particle.beach == 0:
+        dist = fieldset.distance2shore[time, particle.depth, particle.lat,
+                                       particle.lon]
+        if dist < 10:
+            beach_prob = math.exp(-particle.dt/(particle.coastPar*86400.))
+            if ParcelsRandom.random(0., 1.) > beach_prob:
+                particle.beach = 1
+    # Now the part where we build in the resuspension
+    elif particle.beach == 1:
+        resus_prob = math.exp(-particle.dt/(particle.resus_t*86400.))
+        if ParcelsRandom.random(0., 1.) > resus_prob:
+            particle.beach = 0
+    # Update the age of the particle
+    particle.age += particle.dt
+
+
+def AntiBeachNudging(particle, fieldset, time):
+    """
+    The nudging current is 1 m s^-1, which ought to be sufficient to overpower
+    any coastal current (I hope) and push our particle back out to sea so as to
+    not get stuck
+
+    update 11/03/2020: Following tests and discussions with Cleo, the nudging
+    current will now kick in starting at 500m from the coast, since otherwise
+    the particles tended to get stuck if we used the velocity treshhold.
+    """
+
+    particle.distance = fieldset.distance2shore[time, particle.depth,
+                                                particle.lat, particle.lon]
+
+    if fieldset.distance2shore[time, particle.depth,
+                               particle.lat, particle.lon] < 0.5:
+        borUab = fieldset.borU[time, particle.depth, particle.lat,
+                               particle.lon]
+        borVab = fieldset.borV[time, particle.depth, particle.lat,
+                               particle.lon]
+        particle.lon += borUab*particle.dt
+        particle.lat += borVab*particle.dt
+
+
+def AdvectionRK4_floating(particle, fieldset, time):
+    """Advection of particles using fourth-order Runge-Kutta integration.
+    Function needs to be converted to Kernel object before execution
+    A particle only moves if it has not beached (rather obviously)
+    """
+    if particle.beach == 0:
+        (u1, v1) = fieldset.UV[particle]
+        lon1, lat1 = (particle.lon + u1*.5*particle.dt,
+                      particle.lat + v1*.5*particle.dt)
+        (u2, v2) = fieldset.UV[time + .5 * particle.dt,
+                               particle.depth, lat1, lon1, particle]
+        lon2, lat2 = (particle.lon + u2*.5*particle.dt,
+                      particle.lat + v2*.5*particle.dt)
+        (u3, v3) = fieldset.UV[time + .5 * particle.dt,
+                               particle.depth, lat2, lon2, particle]
+        lon3, lat3 = (particle.lon + u3*particle.dt,
+                      particle.lat + v3*particle.dt)
+        (u4, v4) = fieldset.UV[time + particle.dt,
+                               particle.depth, lat3, lon3, particle]
+        particle.lon += (u1 + 2*u2 + 2*u3 + u4) / 6. * particle.dt
+        particle.lat += (v1 + 2*v2 + 2*v3 + v4) / 6. * particle.dt
+
+
+def BrownianMotion2D(particle, fieldset, time):
+    """Kernel for simple Brownian particle diffusion in zonal and meridional
+    direction. Assumes that fieldset has fields Kh_zonal and Kh_meridional
+    we don't want particles to jump on land and thereby beach"""
+    if particle.beach == 0:
+        dWx = ParcelsRandom.normalvariate(0, math.sqrt(math.fabs(particle.dt)))
+        dWy = ParcelsRandom.normalvariate(0, math.sqrt(math.fabs(particle.dt)))
+
+        bx = math.sqrt(2 * fieldset.Kh_zonal[particle])
+        by = math.sqrt(2 * fieldset.Kh_meridional[particle])
+
+        particle.lon += bx * dWx
+        particle.lat += by * dWy
