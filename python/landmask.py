71b5d5036499cfb90dcf67641f3a90b6b5fcbd15
cpierard <c.m.pierard@uu.nl>
2021-03-30 10:49:49 +0200

    update landmask functions for gemini tests.

--- python/landmask.py
+++ python/landmask.py
@@ -4,14 +4,11 @@ import numpy as np
 
 def make_landmask(fielddata, indices):
     """Returns landmask where land = 1 and ocean = 0.
-
     - fielddata is the path to an output of a model (netcdf file expected).
     - indices is a dictionary such as:
         indices = {'lat': slice(1, 900), 'lon': slice(1284, 2460)}.
-
     Output: 2D array containing the landmask. Were the landcells are 1 and
             the ocean cells are 0.
-
     Warning: tested for the CMEMS model outputs where I asume that the variable
             uo exists.
     """
@@ -23,35 +20,11 @@ def make_landmask(fielddata, indices):
     return landmask
 
 
-def make_grid(fielddata, indices):
-    """Returns landmask where land = 1 and ocean = 0
-
-    - fielddata is the path to an output of a model (netcdf file expected).
-    - indices is a dictionary such as:
-        indices = {'lat': slice(1, 900), 'lon': slice(1284, 2460)}.
-
-    Output: 2D masked array array containing the landmask, 1D array with the
-            latitudes and a 1D array with the longitudes.
-
-    Warning: tested for the CMEMS model outputs where I asume that the variable
-            uo exists.
-    """
-    datafile = Dataset(fielddata)
-    landmask = datafile.variables['uo'][0, 0, indices['lat'], indices['lon']]
-    landmask = np.ma.masked_invalid(landmask)
-    lat = datafile.variables['latitude'][:].data
-    lon = datafile.variables['longitude'][:].data
-
-    return landmask, lat, lon
-
-
 def get_coastal_cells(landmask):
     """Function that detects the coastal cells, i.e. the ocean cells directly
     next to land. Computes the Laplacian of landmask.
-
     - landmask: the land mask built using `make_landmask`, where land cell = 1
                 and ocean cell = 0.
-
     Output: 2D array array containing the coastal cells, the coastal cells are
             equal to one, and the rest is zero.
     """
@@ -67,10 +40,8 @@ def get_coastal_cells(landmask):
 def get_shore_cells(landmask):
     """Function that detects the shore cells, i.e. the land cells directly
     next to the ocean. Computes the Laplacian of landmask.
-
     - landmask: the land mask built using `make_landmask`, where land cell = 1
                 and ocean cell = 0.
-
     Output: 2D array array containing the shore cells, the shore cells are
             equal to one, and the rest is zero.
     """
@@ -83,19 +54,24 @@ def get_shore_cells(landmask):
     return shore
 
 
-def create_border_current(landmask):
+def create_border_current(landmask, double_cell=False):
     """Function that creates a border current 1 m/s away from the shore.
-
     - landmask: the land mask built using `make_landmask`.
-
+    - double_cell: Boolean for determining if you want a double cell.
+      Default set to False.
     Output: two 2D arrays, one for each camponent of the velocity.
     """
+    shore = get_shore_cells(landmask)
     coastal = get_coastal_cells(landmask)
     Ly = np.roll(landmask, -1, axis=0) - np.roll(landmask, 1, axis=0)
     Lx = np.roll(landmask, -1, axis=1) - np.roll(landmask, 1, axis=1)
 
-    v_x = np.ma.masked_where(coastal == 0, -Lx)
-    v_y = np.ma.masked_where(coastal == 0, -Ly)
+    if double_cell:
+        v_x = -Lx*(coastal+shore)
+        v_y = -Ly*(coastal+shore)
+    else:
+        v_x = -Lx*(coastal)
+        v_y = -Ly*(coastal)
 
     magnitude = np.sqrt(v_y**2 + v_x**2)
     # the coastal cells between land create a problem. Magnitude there is zero
@@ -105,20 +81,16 @@ def create_border_current(landmask):
 
     v_x = v_x/magnitude
     v_y = v_y/magnitude
-    v_x.set_fill_value(value=0)
-    v_y.set_fill_value(value=0)
 
-    return v_x.data, v_y.data
+    return v_x, v_y
 
 
 def distance_to_shore(landmask, dx=1):
     """Function that computes the distance to the shore. It is based in the
     the `get_coastal_cells` algorithm.
-
     - landmask: the land mask built using `make_landmask` function.
     - dx: the grid cell dimesion. This is a crude approximation of the real
     distance (be careful).
-
     Output: 2D array containing the distances from shore.
     """
     ci = get_coastal_cells(landmask)
@@ -131,22 +103,11 @@ def distance_to_shore(landmask, dx=1):
         landmask_i += ci
         dist += ci*(i+2)
         i += 1
+
     return dist*dx
 
-
-# def distance_to_shore(landmask, iterations=20, dx=1):
-#     ci = get_coastal_cells(landmask)
-#     landmask_i = landmask + ci
-#     dist = ci
-#
-#     for i in range(iterations):
-#         ci = get_coastal_cells(landmask_i)
-#         landmask_i += ci
-#         dist += ci*(i+1)
-#     return dist*dx
-
-
 # Getting my data saved for simulations
+
 
 file_path = '/data/oceanparcels/input_data/CMEMS/' + \
     'GLOBAL_ANALYSIS_FORECAST_PHY_001_024_SMOC/SMOC_20170703_R20170704.nc'
